3ae4596b7f837492eba76fa45a63ecd2f5eee06a
jiazhifeng <double_attack@126.com>
2021-11-25 16:53:13 +0800

    Update files

--- split.py
+++ split.py
@@ -11,19 +11,23 @@ def train_test_split(input_name, stop_val):
     exceed_val = 0
     smallest_graphid = sys.maxsize
     writing_in_train = True
+    read_graphid = True
     for each_line in all_lines:
         num_of_spaces = each_line.count(" ")
         if num_of_spaces == 0:
             content = int(each_line.strip("\n"))
-            if content > exceed_val:
+            if read_graphid:
                 smallest_graphid = min(content, smallest_graphid)
                 count_graphnum += 1
+                read_graphid = False
             if count_graphnum == stop_val:
                 writing_in_train = False
         if num_of_spaces == 1:
             content = each_line.strip("\n").split(" ")
             if int(content[0]) > exceed_val or int(content[1]) > exceed_val:
                 exceed_val = max(max(int(content[0]), int(content[1])), exceed_val)
+        if num_of_spaces == 2 and not read_graphid:
+            read_graphid = True
         if writing_in_train:
             train_file.write(each_line)
         else:
